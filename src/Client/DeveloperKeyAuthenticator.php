831d45e163176ff91604c2016e84fb70958f61fd
Gabriel SULLICE <gabriel@sullice.com>
2017-10-06 12:10:45 +0200

    Fix developer key authenticator

--- src/Client/DeveloperKeyAuthenticator.php
+++ src/Client/DeveloperKeyAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\gapi\Client;
 
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\key\KeyInterface;
 use \Google_Client;
 use Psr\Log\LoggerInterface;
@@ -14,7 +15,7 @@ class DeveloperKeyAuthenticator implements ClientAuthenticatorInterface {
   /**
    * {@inheritdoc}
    */
-  public static function authenticate(Google_Client $client, KeyInterface $key, LoggerInterface $logger) {
+  public static function authenticate(Google_Client $client, KeyInterface $key, ImmutableConfig $config, LoggerInterface $logger) {
     $client->setDeveloperKey($key->getKeyValue());
     return TRUE;
   }
